03339d74fb1e78a6982ad9931fa36e99a9ad8061
Alex <alexwmaustin@gmail.com>
2021-07-02 02:52:07 -0400

    Modified for install script

--- Brawlhalla_AutoStart.bat
+++ Brawlhalla_AutoStart.bat
@@ -2,26 +2,11 @@
 rem HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run for the brawlhalla autostart
 Setlocal EnableDelayedExpansion
 
-IF EXIST Path.txt (
-    goto :loadFile
-) ELSE (
-    goto :savePath
-)
 
-:savePath 
-echo path not initalized
-set /p drv="What drive letter is brawlhalla in? "
-
-for /R "%drv%:\" %%x in ("*Brawlhalla.exe") do set pth="%%~x"
-echo %pth% > Path.txt
-goto :exec
-
-:loadFile
-for /f "tokens=*" %%G in (Path.txt) do (
-  set pth= %%G
-)
+:loadPath
+for /f "tokens=3" %%a in ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\BrawlhallaAutoStart" /V BrawlhallaInstallPath') do set pth=%%a
 
 :exec
-Start "" "E:\Steam\steamapps\common\Brawlhalla\Brawlhalla.exe" 
+Start "" "%pth%" 
 TIMEOUT /T 60
 taskkill /F /IM Brawlhalla.exe
